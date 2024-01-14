64adc765b4465137091e767d57c440425865e13b
MMikilanjelo <144453227+MMikilanjelo@users.noreply.github.com>
2024-01-14 14:41:57 +0200

    added state machine

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -1,6 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
@@ -8,13 +5,13 @@ public class CameraController : MonoBehaviour
     [SerializeField] private Vector3 offset;
     [SerializeField] private float _damping;
     public Transform target;
-    
+     
     private Vector3 vel = Vector3.zero;
-
     private void FixedUpdate()
     {
         Vector3 targetPosition = target.position + offset;
         targetPosition.z  = transform.position.z;
-        transform.position = Vector3.SmoothDamp(transform.position , targetPosition , ref vel , _damping);  
+        transform.position = Vector3.SmoothDamp(transform.position , targetPosition , ref vel , _damping);
     }
+
 }
